e3a0dc3c8c3f1b6750055c005995c8d26cf4dc7a
UmerIftikhar <uiftikhar1@gmail.com>
2015-11-24 20:27:11 +0200

    Update app.js

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ var mailer = require("nodemailer");
 var morgan = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var session = require('express-session');
+//var session = require('express-session');
 
 var configDB = require('./config/database.js');
 app.use('/js', express.static(__dirname + '/client/js'));
@@ -38,7 +38,7 @@ app.use(bodyParser()); // get information from html forms
 app.set('view engine', 'ejs'); // set up ejs for templating
 
 // required for passport
-app.use(session({ secret: 'ilovescotchscotchyscotchscotch' })); // session secret
+app.use(express.session({ secret: 'ilovescotchscotchyscotchscotch' })); // session secret
 app.use(passport.initialize());
 app.use(passport.session()); // persistent login sessions
 app.use(flash()); // use connect-flash for flash messages stored in session
